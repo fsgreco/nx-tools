07a2ef984e0be8ed3dea912b3b9c279b382d647f
Santiago <fsgreco@fastmail.com>
2024-03-29 01:14:58 +0100

    One script for bin and postinstall

--- packages/nx-tool/initial-setup.ts
+++ packages/nx-tool/initial-setup.ts
@@ -3,8 +3,12 @@ import { FsTree } from "nx/src/generators/tree"
 import { readJsonFile /* , updateJson  */} from "@nx/devkit"
 import { spawnSync } from "node:child_process"
 
+//const projectDir = process.env.INIT_CWD || path.resolve('../../') || path.dirname( path.dirname( process.cwd() ) )
+const projectDir = process.env.INIT_CWD || process.cwd()
 
-const tree = new FsTree( process.cwd(), false )
+process.chdir(projectDir)
+
+const tree = new FsTree( projectDir, false )
 
 // If both of this are false then hic sunt leones (it is not an Nx project)
 const isNxProject = tree.exists('nx.json') 
@@ -17,7 +21,7 @@ const packageJson = readJsonFile(tree.root + '/package.json') satisfies Record<s
 const hasNx = packageJson.hasOwnProperty('nx')
 const hasWorkspaces = packageJson.hasOwnProperty('workspaces')
 
-const parseExitCode = (code:number | null) => code === 0 ? '✅' : '🔴'
+const parseExitCode = (code: number | null) => code === 0 ? '✅' : '🔴'
 
 if (!isNxProject && !hasProjectJson && !hasWorkspaces && !hasNx ) {
 	//updateJson(tree, 'package.json', (json) => { json.nx = {}; return json; } )
@@ -28,7 +32,7 @@ if (!isNxProject && !hasProjectJson && !hasWorkspaces && !hasNx ) {
 const hasScript = packageJson?.scripts.hasOwnProperty("new:component")
 if (!hasScript) {
 	const result = spawnSync('npm',['pkg','set','scripts.new:component=npx nx g nx-tool:new-component'])
-	console.log(parseExitCode(result?.status) + ' Add new script on package.json')
+	console.log(parseExitCode(result.status) + ' Add new script on package.json')
 }
 
 console.log('✅ All done.')
